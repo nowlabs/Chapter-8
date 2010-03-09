8b6f86b75461a617f864a1b9573e56c042a704aa
Thomas Eapen <thomas@nowlabs.net>
2010-03-08 19:02:55 -0500

    Working with NSMutableDictionary & NSArray

--- Chapter 8.m
+++ Chapter 8.m
@@ -2,9 +2,16 @@
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-
-    // insert code here...
-    NSLog(@"Hello, World!");
+	NSArray *array = 
+	[NSArray arrayWithObjects:@"one", @"two", @"three", nil];
+	for (int i = 0; i < [array count]; i++) {
+		NSLog(@"index %d has %@", i, [array objectAtIndex:i]);
+	}
+	//id noObject = [array objectAtIndex:28000]; /* Global breakpoint testing */
+	NSNumber *number = [NSNumber numberWithInt:42];
+	NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+	[dictionary setValue:number forKey:@"Bork"];
+	NSLog(@"Key: %@, Value: %d", @"Bork", [[dictionary valueForKey:@"Bork"] intValue]); 
     [pool drain];
     return 0;
 }
