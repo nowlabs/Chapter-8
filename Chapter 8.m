329a30d2c0e5427593c065bf09f8dfd878daf8f2
Thomas Eapen <thomas@nowlabs.net>
2010-03-08 19:39:13 -0500

    Working with NSValue, NSRect, NSNumber

--- Chapter 8.m
+++ Chapter 8.m
@@ -8,10 +8,19 @@ int main (int argc, const char * argv[]) {
 		NSLog(@"index %d has %@", i, [array objectAtIndex:i]);
 	}
 	//id noObject = [array objectAtIndex:28000]; /* Global breakpoint testing */
+	
 	NSNumber *number = [NSNumber numberWithInt:42];
 	NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
 	[dictionary setValue:number forKey:@"Bork"];
-	NSLog(@"Key: %@, Value: %d", @"Bork", [[dictionary valueForKey:@"Bork"] intValue]); 
+	NSLog(@"Key: %@, Value: %d", @"Bork", [[dictionary valueForKey:@"Bork"] intValue]);
+	
+	NSMutableArray *marray = [NSMutableArray array];
+	NSRect rect = NSMakeRect(2, 4, 30, 40);
+	NSValue *value = [NSValue valueWithRect:rect];
+	[marray addObject:value];
+	value = [marray objectAtIndex:0];
+	NSRect anotherrect = [value rectValue]; 
+	NSLog(@"Origin of Rectangle is: %f", anotherrect.origin.x);
     [pool drain];
     return 0;
 }
